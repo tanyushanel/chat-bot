627cf22435deaeacf866026db66f1cee23c747fd
tanyushanel <tatyana.nelipovich@gmail.com>
2023-11-06 14:08:42 +0100

    refactor: splitted vars to external file options

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const TelegramAPI = require("node-telegram-bot-api");
 
+const { againOptions, gameOptions } = require("./options");
+
 const token = "6434499783:AAGQF-NLujUvt_YO8xjhIwJqf-PdX3BBBuU";
 
 const bot = new TelegramAPI(token, { polling: true });
 
 const chats = {};
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: "1", callback_data: "1" }],
-      [{ text: "2", callback_data: "2" }],
-      [{ text: "3", callback_data: "3" }],
-      [{ text: "4", callback_data: "4" }],
-      [{ text: "5", callback_data: "5" }],
-      [{ text: "6", callback_data: "6" }],
-      [{ text: "7", callback_data: "7" }],
-      [{ text: "8", callback_data: "8" }],
-      [{ text: "9", callback_data: "9" }],
-      [{ text: "0", callback_data: "0" }],
-    ],
-  }),
-};
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "Start again", callback_data: "/again" }]],
-  }),
-};
 
 const startGame = async (chatId) => {
   const randNumber = Math.floor(Math.random() * 10);
